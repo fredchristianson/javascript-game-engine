37dcffec081a4614f8c96d80b96f2b76538d02e4
Fred Christianson <fred@devrelief.com>
2023-03-18 06:59:36 -0400

    fixed example in UTIL.join

--- browser/modules/helpers/util.js
+++ browser/modules/helpers/util.js
@@ -50,7 +50,7 @@ const UTIL = {
   * 
   * would return 
   * 
-  * [ [a,1], [b,2], [c,3]]
+  * [ [a,1],[a,2],a[3], [b,1],[b,2],[b,3], [c,1], [c,2][c,3]]
   * 
   * @param {Iterable}  listA the first list
   * @param {Iterable} listB the second list
